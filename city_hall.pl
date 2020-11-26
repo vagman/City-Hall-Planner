58976ac6e270a351d4dbec11873ad844281382ea
Vaggelis Manousakis <bmanousakis@gmail.com>
2020-11-27 01:44:48 +0200

    just getting started

---
+++ city_hall.pl
@@ -0,0 +1,5 @@
+family(
+    person(tom, fox, date(7, may, 1950), works(bbc, 15200)),
+    person(ann, fox, date(9, may, 1951), unemployed),
+    [person(pat, fox, date(5, may, 1973), unemployed),
+    person(jim, fox, date(5, may, 1973), unemployed)]).
