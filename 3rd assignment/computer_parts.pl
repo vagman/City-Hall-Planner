07b13bd369a15abc6d20f1db70516ca786a21d0b
Vaggelis Manousakis <bmanousakis@gmail.com>
2020-12-30 14:15:41 +0200

    ongoing implementation

--- 3rd assignment/computer_parts.pl
+++ 3rd assignment/computer_parts.pl
@@ -1,18 +1,44 @@
 computer(
         motherboard(
-            cpu_chip(), 
-            ram_slots(), 
-            southbridge_and_northbridge(), 
-            bios(), 
-            ioports()),
-        cpu(memory_of_storage_unit(), 
-            control_unit(), 
-            arithmetic_logic_unit()), 
-        gpu(graphics_processor(), video_memory(), ramdac(), interface()), 
-        case, 
-        memory, 
-        storage, 
-        power_supply, 
-        optical_drive, 
-        cooling).
+            cpu_chip(metal,), 
+            ram_slots(metal), 
+            southbridge_and_northbridge(metal,), 
+            bios(metal,), 
+            ioports(plastic)),
+
+        cpu(
+            memory_of_storage_unit(metal), 
+            control_unit(metal,), 
+            arithmetic_logic_unit(metal,),
+            fan(plastic,)), 
+
+        gpu(
+            graphics_processor(metal,), 
+            video_memory(metal), 
+            ramdac(metal,), 
+            interface(plastic)), 
+
+        case(), 
+
+        memory(), 
+
+        storage(), 
+
+        power_supply(
+            cable(plastic),
+            fan(plastic,),
+            transformer(metal,),
+        ), 
+
+        optical_drive(
+            cable(plastic),
+            laser_beam(metal,)
+            lens(metal,)
+            photo_diode(plastic,)
+
+
+        ), 
+
+        cooling()
+        ).
 
