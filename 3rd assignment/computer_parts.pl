af68caf6c848ff910e307f45cf8f1de9c99a4c77
vagman <bmanousakis@gmail.com>
2024-01-09 15:02:31 +0200

    WIP: first attempt on 3rd assignment

--- 3rd assignment/computer_parts.pl
+++ 3rd assignment/computer_parts.pl
@@ -1,44 +1,40 @@
-computer(
-        motherboard(
-            cpu_chip(metal,), 
-            ram_slots(metal), 
-            southbridge_and_northbridge(metal,), 
-            bios(metal,), 
-            ioports(plastic)),
+% Facts stating the components of the personal computer
+component(computer, [motherboard, power_supply, display, storage]).
+component(motherboard, [cpu, ram, chipset]).
+component(power_supply, [cable, fan, transformer]).
+component(transformer, [wire, core]).
 
-        cpu(
-            memory_of_storage_unit(metal), 
-            control_unit(metal,), 
-            arithmetic_logic_unit(metal,),
-            fan(plastic,)), 
+% Facts stating the materials of components
+made_of(cable, metal).
+made_of(fan, plastic).
+made_of(transformer, metal).
+made_of(wire, metal).
+made_of(core, metal).
+made_of(cpu, metal).
+made_of(ram, metal).
+made_of(chipset, metal).
+made_of(storage, plastic).
+made_of(display, plastic).
 
-        gpu(
-            graphics_processor(metal,), 
-            video_memory(metal), 
-            ramdac(metal,), 
-            interface(plastic)), 
+% Question 1: Rules for finding components and their sub-components
+sub_component(X, Y) :- 
+    component(Y, Components),
+    member(X, Components).
 
-        case(), 
+sub_component(X, Y) :- 
+    component(Z, Components),
+    member(Z, Components),
+    sub_component(X, Z).
 
-        memory(), 
+% Question 2: Rules for finding components that contain metal or plastic
+contains_material(X, Material) :- 
+    made_of(X, Material).
+    
+contains_material(X, Material) :- 
+    component(Y, Components),
+    member(X, Components),
+    contains_material(Y, Material).
 
-        storage(), 
-
-        power_supply(
-            cable(plastic),
-            fan(plastic,),
-            transformer(metal,),
-        ), 
-
-        optical_drive(
-            cable(plastic),
-            laser_beam(metal,)
-            lens(metal,)
-            photo_diode(plastic,)
-
-
-        ), 
-
-        cooling()
-        ).
-
+% Question 3: Rule for finding the set of components that make up the object
+subcomponents_of_components(Object, Components) :- 
+    component(Object, Components).
